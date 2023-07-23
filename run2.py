19e886f61ff55335fb8bf5daea8ea187b74fa74d
ildar <idr-ildar@yandex.ru>
2023-07-23 15:03:50 +0000

    remove silence

--- run2.py
+++ run2.py
@@ -14,6 +14,7 @@ from replica import utils as replica
 from bark.generation import SAMPLE_RATE
 from df.enhance import enhance, load_audio, save_audio
 from pydub import AudioSegment
+from pydub.silence import split_on_silence
 
 if not os.path.exists("temp"):
     os.makedirs("temp")
@@ -42,15 +43,15 @@ if __name__ == "__main__":
     audio = AudioSegment.from_file(input_file)
     duration_in_s = len(audio) / 1000
 
-    DURATION_LIMIT = 20
+    DURATION_LIMIT = 50
     START_POSITION = random.randint(0, int(duration_in_s - DURATION_LIMIT))
     print("===== START_POSITION:", START_POSITION, "=====")
     if (duration_in_s > DURATION_LIMIT):
-        cmd = f"ffmpeg -y -i {input_file} -c copy -ss {START_POSITION} -t {DURATION_LIMIT} temp/input_cut.mp4"
+        cmd = f"ffmpeg -y -i {input_file} -ss {START_POSITION} -t {DURATION_LIMIT} temp/input_cut.mp4"
         subprocess.run(cmd.split())
         input_file = "temp/input_cut.mp4"
 
-    cmd = f"ffmpeg -y -i {input_file} -c copy temp/input_video.mp4 temp/input_audio.wav"
+    cmd = f"ffmpeg -y -i {input_file} temp/input_video.mp4 temp/input_audio.wav"
     subprocess.run(cmd.split())
     
     df_model, df_state = replica.voice_cleaning_setup()
@@ -72,11 +73,11 @@ if __name__ == "__main__":
     audio = AudioSegment.from_file("temp/clean_audio.wav")
     duration_in_s = len(audio) / 1000
     for i in range(int(duration_in_s // 10)):
-        cmd = f"ffmpeg -y -i temp/input_video.mp4 -c copy -ss {i*10} -t 10 temp/input_video_cut.wav"
+        cmd = f"ffmpeg -y -i temp/input_video.mp4 -ss {i*10} -t 10 temp/input_video_cut.wav"
         subprocess.run(cmd.split())
-        cmd = f"ffmpeg -y -i temp/clean_audio.wav -c copy -ss {i*10} -t 10 temp/clean_audio_cut.wav"
+        cmd = f"ffmpeg -y -i temp/clean_audio.wav -ss {i*10} -t 10 temp/clean_audio_cut.wav"
         subprocess.run(cmd.split())
-        cmd = f"ffmpeg -y -i temp/bg_audio.wav -c copy -ss {i*10} -t 10 temp/bg_audio_cut.wav"
+        cmd = f"ffmpeg -y -i temp/bg_audio.wav -ss {i*10} -t 10 temp/bg_audio_cut.wav"
         subprocess.run(cmd.split())
         
         whisper_model = replica.transcribe_audio_setup("medium")
@@ -96,10 +97,21 @@ if __name__ == "__main__":
         del tts
         replica.clear_memory()
 
-        bg_audio = AudioSegment.from_file("temp/bg_audio_cut.wav")
         converted_voice = AudioSegment.from_file("temp/converted_voice.wav")
-        converted_voice_speed = converted_voice.speedup(playback_speed=len(converted_voice)/len(bg_audio))
-        combined_voice_bg = bg_audio.overlay(converted_voice_speed)
+        chunks = split_on_silence(converted_voice, 
+            min_silence_len = 200,
+            silence_thresh = -40,
+            keep_silence=200,
+        )
+        joined_chunks = sum(chunks)
+        joined_chunks.export("temp/converted_voice_cut.wav", format="wav")
+
+        bg_audio = AudioSegment.from_file("temp/bg_audio_cut.wav")
+        converted_voice = AudioSegment.from_file("temp/converted_voice_cut.wav")
+        speed_rate = len(converted_voice)/len(bg_audio)
+        if (speed_rate > 1.0):
+            converted_voice = converted_voice.speedup(playback_speed=speed_rate)
+        combined_voice_bg = bg_audio.overlay(converted_voice)
         combined_voice_bg.export("temp/combined_voice_bg.wav", format='wav')
         
         replica.video_synchronization_setup()
@@ -110,11 +122,11 @@ if __name__ == "__main__":
         for video_file in video_files:
             file.write(f"file '{video_file}'\n")
     
-    cmd = f"ffmpeg -y -f concat -safe 0 -i temp/filelist.txt -c copy {args.output_file}"
+    cmd = f"ffmpeg -y -f concat -safe 0 -i temp/filelist.txt {args.output_file}"
     subprocess.run(cmd.split())
 
     #for i in range(int(audio_duration // 10)):
-    #    cmd = f"ffmpeg -y -i {input_file} -c copy -ss {i*10} -t 10 temp/input_video.mp4 -ss {i*10} -t 10 temp/input_audio.wav"
+    #    cmd = f"ffmpeg -y -i {input_file} -ss {i*10} -t 10 temp/input_video.mp4 -ss {i*10} -t 10 temp/input_audio.wav"
     #    subprocess.run(cmd.split())
     #    df_model, df_state = replica.voice_cleaning_setup()
     #    replica.clean_audio(df_model, df_state, "temp/input_audio.wav", "temp/clean_audio.wav")
@@ -140,5 +152,5 @@ if __name__ == "__main__":
     #    replica.video_synchronization_setup()
     #    replica.sync_video("temp/input_video.mp4", "temp/converted_voice.wav", f"temp/output_video_{i:03n}.mp4")
 #
-    #cmd = f"ffmpeg -y -f concat -i temp/output_video_%03d.mp4 -c copy {args.output_file}"
+    #cmd = f"ffmpeg -y -f concat -i temp/output_video_%03d.mp4 {args.output_file}"
     #subprocess.run(cmd.split())
